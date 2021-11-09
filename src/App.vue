0f6b5ea22279da28fc30a1c39a226c737464a496
Everton Nnunes <everton.code@gmail.com>
2021-11-09 11:20:04 -0300

    feat: add new message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to the little project"/>
+    <HelloWorld msg="Hi there!"/>
   </div>
 </template>
 
