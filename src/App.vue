fc1cd5c8478ba7843cb58764cec48830fe1f9b80
Everton Nnunes <everton.code@gmail.com>
2021-11-09 11:18:26 -0300

    fix: fix color to app

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #065cb3;
   margin-top: 60px;
 }
 </style>
